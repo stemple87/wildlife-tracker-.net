24a58d306cbee4802c3a8351cd4c587a515a4eee
Simon Temple <simonctemple@live.com>
2016-04-18 16:14:30 -0700

    Add all crud functionality

--- src/wildlife_tracker/Controllers/AnimalsController.cs
+++ src/wildlife_tracker/Controllers/AnimalsController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using wildlife_tracker.Models;
-
+using Microsoft.Data.Entity;
 
 namespace wildlife_tracker.Controllers
 {
@@ -15,6 +15,53 @@ namespace wildlife_tracker.Controllers
         public IActionResult Index()
         {
             return View(db.Animals.ToList());
+        }
+        public IActionResult Details(int id)
+        {
+            var thisAnimal = db.Animals.FirstOrDefault(x => x.AnimalId == id);
+            return View(thisAnimal);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Animal item)
+        {
+            db.Animals.Add(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisAnimal = db.Animals.FirstOrDefault(x => x.AnimalId == id);
+            return View(thisAnimal);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Animal item)
+        {
+            db.Entry(item).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var thisAnimal = db.Animals.FirstOrDefault(x => x.AnimalId == id);
+            return View(thisAnimal);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisAnimal = db.Animals.FirstOrDefault(x => x.AnimalId == id);
+            db.Animals.Remove(thisAnimal);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
